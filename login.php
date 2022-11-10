08cac815484c9b96c8b814b958fdee886b4b5efe
Vũ Nguyễn <nguyenvu001x@gmail.com>
2022-11-10 16:27:47 +0700

    Update 4:26 chiều 10/11

--- login.php
+++ login.php
@@ -2,29 +2,6 @@
 require "connect.php";
 session_start();
 ?>
-<!-- <!DOCTYPE>
-<html lang="en">
-
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Đăng Nhập</title>
-</head>
-
-<body>
-    <div id="login">
-        <h1>Đăng Nhập</h1>
-        <div id="login-body">
-            <form action="" method="POST">
-                <Label class="login-label">Tên Tài Khoản</Label>
-                <input type="text" id="username" name="username" placeholder="Username" required>
-                <Label class="login-label">Mật Khẩu</Label>
-                <input type="password" id="password" name="password" placeholder="Password" required>
-                <input type="submit" id="login-btn" name="login" value="Đăng Nhập">
-                <a href="register.php" id="register-btn" name="register">Đăng Ký</a>
-            </form>
-        </div>
-    </div> -->
 <?php
 if (isset($_POST['submit'])) {
   $tk = $_POST['username'];
@@ -45,18 +22,13 @@ if (isset($_POST['submit'])) {
         header("location:userset.php");
       else header("location:adminset.php");
     }
-    // header("location:index.php");
-    // setcookie("success", "Đăng nhập thành công!", time() + 1, "/", "", 0);
+
   } else {
     echo "Tài khoản hoặc mật khẩu không đúng !";
-    // header("location:index.php");
-    // setcookie("error", "Đăng nhập không thành công!", time() + 1, "/", "", 0);
+
   }
 }
 ?>
-<!-- </body>
-
-</html> -->
 
 <!DOCTYPE html>
 <html lang="en">
