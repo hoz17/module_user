8c38041a332501267859b31b29ebbcba19814b92
Irinyan <sn992002@gmail.com>
2022-11-12 16:30:59 +0700

    Update update.php

--- update.php
+++ update.php
@@ -15,9 +15,13 @@ session_start();
                 $ID = $_GET["ID"];
             }
 
-    $sql = "SELECT * FROM user WHERE ID = '$ID'";
-    $result = mysqli_query($conn, $sql);
-    $rows = mysqli_fetch_assoc($result);
+            $sql4 = "SELECT * FROM user WHERE ID = " . $_SESSION['online'];
+            $result4 = mysqli_query($conn, $sql4);
+            $rows4 = mysqli_fetch_assoc($result4);
+
+            $sql = "SELECT * FROM user WHERE ID = '$ID'";
+            $result = mysqli_query($conn, $sql);
+            $rows = mysqli_fetch_assoc($result);
 
             if (isset($_POST['update'])) {
                 $Fullname = $_POST["Fullname"];
@@ -44,53 +48,56 @@ session_start();
                 }
             }
 
-    ?>
-    <form method="POST" action="">
-        <script>
-            function showPwd() {
-                var x = document.getElementById("myInput");
-                if (x.type === "password") {
-                    x.type = "text";
-                } else {
-                    x.type = "password";
-                }
-            }
-        </script>
-        <p class="minititle">Username</p><input type="text" name="Username" value="<?php echo $rows['Username']; ?>" <?php if ($rows4['Status'] != 3) echo 'readonly'; ?> /><br /><br />
-        <p class="minititle" <?php if ($rows4['Status'] != 3) echo 'hidden'; ?>>Password</p><input type="password" id="myInput" name="Password" <?php if ($rows['Status'] != 3) echo 'hidden'; ?> value="<?php echo $rows['Password']; ?>" required /><input <?php if ($rows['Status'] != 3) echo 'hidden'; ?> type="checkbox" onclick="showPwd()" name="" id="">
-        <div <?php if ($rows['Status'] != 3) echo 'hidden'; ?>>Hiện mật khẩu</div>
-        <?php if ($rows['Status'] != 1) echo '<br><br>'; ?>
-        <p class="minititle">Email</p><input type="text" name="Email" value="<?php echo $rows['Email']; ?>" required <?php if ($rows['Status'] != 3) echo 'readonly'; ?> /><br /><br />
-        <p class="minititle" <?php if ($rows['Status'] == 1) echo 'hidden'; ?>>Trạng thái</p>
-        <select name="cboStatus" id="cboStatus" <?php if ($rows['Status'] == 1) echo 'hidden'; ?>>
-            <option value="0" <?php if ($rows['Status'] == 0) echo "selected"; ?>>Chưa kích hoạt</option>
-            <option value="1" <?php if ($rows['Status'] == 1) echo "selected"; ?>>User</option>
-            <option value="2" <?php if ($rows['Status'] == 2) echo "selected"; ?>>Blocked</option>
-            <option value="3" <?php if ($rows['Status'] == 3) echo "selected"; ?>>Admin</option>
-        </select>
-        <?php if ($rows['Status'] != 1) echo '<br><br>'; ?>
-        <p class="minititle">Họ tên</p><input type="text" name="Fullname" value="<?php echo $rows['Fullname']; ?>" required /><br /><br />
-        <p class="minititle">Ngày Sinh</p><input type="date" name="Birthday" value="<?php echo $rows['Birthday']; ?>" required /><br /><br />
-        <p class="minititle">Giới Tính</p>
-        <input class="radio" type="radio" name="Gender" value="0" <?php if ($rows["Gender"] == 0) echo "checked"; ?> /> Nam <br /><br />
-        <input class="radio" type="radio" name="Gender" value="1" <?php if ($rows["Gender"] == 1) echo "checked"; ?> /> Nữ<br /><br />
-        <p class="minititle">Địa Chỉ</p>
-        <input type="text" name="Address" value="<?php echo $rows['Address']; ?>" required /><br /><br />
-        <p class="minititle">Nhóm</p>
-        <select name="cboClass" id="cboClass">
-            <?php
-            $conn = mysqli_connect("localhost", "root", "", "module_user");
-            if (!$conn) {
-                echo "Ket noi khong thanh cong";
-                exit();
-            } else {
-                $sql2 = "select * from class";
-                $result2 = mysqli_query($conn, $sql2);
-                if (mysqli_num_rows($result2) > 0) {
-                    while ($row = mysqli_fetch_assoc($result2)) {
-                        $select = "";
-                        if ($rows['Class_id'] == $row['ID']) $select = "selected";
-                        echo "<option " . $select . " value = '" . $row["ID"] . "' >" . $row['Class_name'] . "</option>";
+            ?>
+            <form method="POST" action="">
+                <script>
+                    function showPwd() {
+                        var x = document.getElementById("myInput");
+                        if (x.type === "password") {
+                            x.type = "text";
+                        } else {
+                            x.type = "password";
+                        }
+                    }
+                </script>
+                <p class="minititle">Username</p><input type="text" name="Username" value="<?php echo $rows['Username']; ?>" <?php if ($rows4['Status'] != 3) echo 'readonly'; ?> /><br /><br />
+                <p class="minititle" <?php if ($rows4['Status'] != 3) echo 'hidden'; ?>>Password</p><input type="password" id="myInput" name="Password" <?php if ($rows4['Status'] != 3) echo 'hidden'; ?> value="<?php echo $rows['Password']; ?>" required />
+                <input <?php if ($rows4['Status'] != 3) echo 'hidden'; ?> type="checkbox" onclick="showPwd()" name="" id="">
+                <div <?php if ($rows4['Status'] != 3) echo 'hidden'; ?>>Hiện mật khẩu</div>
+                <?php if ($rows4['Status'] != 1) echo '<br><br>'; ?>
+                <p class="minititle">Email</p><input type="text" name="Email" value="<?php echo $rows['Email']; ?>" required <?php if ($rows4['Status'] != 3) echo 'readonly'; ?> /><br /><br />
+                <p class="minititle" <?php if ($rows4['Status'] == 1) echo 'hidden'; ?>>Trạng thái</p>
+                <select name="cboStatus" id="cboStatus" <?php if ($rows4['Status'] == 1) echo 'hidden'; ?>>
+                    <option value="0" <?php if ($rows['Status'] == 0) echo "selected"; ?>>Chưa kích hoạt</option>
+                    <option value="1" <?php if ($rows['Status'] == 1) echo "selected"; ?>>User</option>
+                    <option value="2" <?php if ($rows['Status'] == 2) echo "selected"; ?>>Blocked</option>
+                    <option value="3" <?php if ($rows['Status'] == 3) echo "selected"; ?>>Admin</option>
+                </select>
+                <?php if ($rows4['Status'] != 1) echo '<br><br>'; ?>
+                <p class="minititle">Họ tên</p><input type="text" name="Fullname" value="<?php echo $rows['Fullname']; ?>" required /><br /><br />
+                <p class="minititle">Ngày Sinh</p><input type="date" name="Birthday" value="<?php echo $rows['Birthday']; ?>" required /><br /><br />
+                <p class="minititle">Giới Tính</p>
+                <input class="radio" type="radio" name="Gender" value="0" <?php if ($rows["Gender"] == 0) echo "checked"; ?> /> Nam <br /><br />
+                <input class="radio" type="radio" name="Gender" value="1" <?php if ($rows["Gender"] == 1) echo "checked"; ?> /> Nữ<br /><br />
+                <p class="minititle">Địa Chỉ</p>
+                <input type="text" name="Address" value="<?php echo $rows['Address']; ?>" required /><br /><br />
+                <p class="minititle">Nhóm</p>
+                <select name="cboClass" id="cboClass">
+                    <?php
+                    $conn = mysqli_connect("localhost", "root", "", "module_user");
+                    if (!$conn) {
+                        echo "Ket noi khong thanh cong";
+                        exit();
+                    } else {
+                        $sql2 = "select * from class";
+                        $result2 = mysqli_query($conn, $sql2);
+                        if (mysqli_num_rows($result2) > 0) {
+                            while ($row = mysqli_fetch_assoc($result2)) {
+                                $select = "";
+                                if ($rows['Class_id'] == $row['ID']) $select = "selected";
+                                echo "<option " . $select . " value = '" . $row["ID"] . "' >" . $row['Class_name'] . "</option>";
+                            }
+                        }
                     }
                     ?>
                 </select><br><br>
